f5bbe4c482ecb529d04b27b26bd873799ac5a903
WairimuMaringa <makena96wairimu@gmail.com>
2022-08-23 15:23:37 -0700

    advanced task two

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    str_cpy = ""
+    for i in range(0, len(str)):
+        if i == n:
+            str_cpy += ""
+        else:
+            str_cpy += str[i]
+            return (str_cpy)
