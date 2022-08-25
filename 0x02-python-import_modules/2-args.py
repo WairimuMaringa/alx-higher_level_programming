5a8f8796f12e3441cdf8c84d30b05ec5aa2c7303
WairimuMaringa <makena96wairimu@gmail.com>
2022-08-25 03:12:49 -0700

    task two

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    len_argv = len(sys.argv)
+    argument = "arguments:"
+    if len_argv == 1:
+        argument = "arguments."
+        if len_argv == 2:
+            argument = "argument:"
+            print("{0} {1}".format(len_argv - 1, argument))
+            for i in range(1, len_argv):
+                print("{0}: {1}".format(i, sys.argv[i]))
