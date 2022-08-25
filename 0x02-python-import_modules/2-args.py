aba858f4f97c7251df1f5bf233dda50251b3c00e
WairimuMaringa <makena96wairimu@gmail.com>
2022-08-25 03:38:27 -0700

    task twoo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    len_argv = len(sys.argv)
-    argument = "arguments:"
-    if len_argv == 1:
-        argument = "arguments."
-        if len_argv == 2:
-            argument = "argument:"
-            print("{0} {1}".format(len_argv - 1, argument))
-            for i in range(1, len_argv):
-                print("{0}: {1}".format(i, sys.argv[i]))
+    len_argv = len(sys.argv) - 1
+
+    if len_argv == 0:
+        print("{} arguments.".format(len_argv))
+    elif len_argv == 1:
+        print("{} argument:".format(len_argv))
+    else:
+        print("{} arguments:".format(len_argv))
+
+        if len_argv >= 1:
+            len_argv = 0
+            for arg in sys.argv:
+                if len_argv != 0:
+                    print("{}: {}".format(i, arg))
+                    i += 1
