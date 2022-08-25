b407ef9d49eee8beeaa98c9006981550c38f7d7c
WairimuMaringa <makena96wairimu@gmail.com>
2022-08-25 11:25:29 -0700

    task twoo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("{} argument:".format(args - 1))
     else:
         print("{} arguments:".format(args - 1))
-        for x in range(1, args):
-            print("{}: {}".format(x, sys.argv[x]))
+    for x in range(1, args):
+        print("{}: {}".format(x, sys.argv[x]))
