3f812504b19bfe04953797e6e9a3960b5e1a11d4
WairimuMaringa <makena96wairimu@gmail.com>
2022-08-25 07:21:05 -0700

    advanced task one

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
                 print("{} - {} = {}".format(a, b, sub(a, b)))
             elif op == '*':
                 print("{} * {} = {}".format(a, b, mul(a, b)))
-            elif op == '/':
+            else:
                 print("{} / {} = {}".format(a, b, div(a, b)))
