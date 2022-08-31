ce0a33c835652fcbf7a6ae213601a23f04f49463
WairimuMaringa <makena96wairimu@gmail.com>
2022-08-31 00:52:26 -0700

    advanced task  one

---
+++ 0x04-python-more_data_structures/13-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num1 = 0
+    num2 = 0
+    for tup in my_list:
+        num1 += tup[0] * tup[1]
+        num2 += tup[1]
+    return (num1 / num2)
