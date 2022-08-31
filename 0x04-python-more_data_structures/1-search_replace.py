eeea82912ab179d7a00ea85cc669eb884d7609b0
WairimuMaringa <makena96wairimu@gmail.com>
2022-08-31 13:45:14 -0700

    task onee

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for i in range(len(new_list)):
-        new_list[i] = replace if my_list[i] == search else my_list[i]
-        return (new_list)
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
