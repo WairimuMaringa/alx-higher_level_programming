72319a4c08cf1cb3d5dcd07856d522a83294c5ed
WairimuMaringa <makena96wairimu@gmail.com>
2022-09-14 06:42:39 -0700

    task zero

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""An empty class Square that defines a square.
+"""
+
+
 class Square:
-    """Class Square that defines a square"""
+    """Class Square empty.
+    """
     pass
