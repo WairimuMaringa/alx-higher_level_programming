49394af9744e7fbd7d54f206fc91ae688a2a1bcf
WairimuMaringa <makena96wairimu@gmail.com>
2022-09-14 08:07:17 -0700

    task three

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Class Square that defines a square by size.
+"""
+
+
+class Square:
+    """Class Square
+    """
+    def __init__(self, size=0):
+        """Method: initialization.
+
+        Args:
+        size(int): size of the square
+        self(class): the class
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
+
+
+def area(self):
+    """Method: returns the square area
+
+    Args:
+    self(class): the class
+    """
+    return (self.__size ** 2)
