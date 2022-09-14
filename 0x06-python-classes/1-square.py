1cd738a9d96f55064be442e44839d4de220face3
WairimuMaringa <makena96wairimu@gmail.com>
2022-09-14 07:01:10 -0700

    task one

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class Square that defines a square.
+"""
+
+
+class Square:
+    """Class Square
+    """
+    def __init__(self, size):
+        """Initialize method that stores the size of the square.
+
+        Args:
+        size(int): size of the square
+        self(class): the class
+        """
+        self.__size = size
