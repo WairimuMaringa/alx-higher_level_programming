d37a1dbc1e8e5847d5fc0000fb599019bc2b0020
WairimuMaringa <makena96wairimu@gmail.com>
2022-09-19 07:29:10 -0700

    task six

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -31,6 +31,7 @@ class Rectangle:
         return ("Rectangle({:d}, {:d})".format(self.width, self.height))
 
     def __del__(self):
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
     def area(self):
