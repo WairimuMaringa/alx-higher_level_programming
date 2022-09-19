3eeb6301412892b96756d721d55e2058ca1abecf
WairimuMaringa <makena96wairimu@gmail.com>
2022-09-19 06:29:49 -0700

    task two

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Defines an object rectangle.
+"""
+
+
+class Rectangle:
+    """Class Rectangle empty.
+    """
+    def __init__(self, width=0, height=0):
+        """Method: initialize.
+
+        Args:
+        width(int): width of rectangle
+        height(int): height of rectangle
+        """
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.height == 0 or self.width == 0:
+            return (0)
+        return ((2 * self.width) + (2 * self.height))
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
