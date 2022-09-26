64d471b34d03f848247da244f6a382be5f392720
WairimuMaringa <makena96wairimu@gmail.com>
2022-09-26 10:59:48 -0700

    task eleven

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Class Square that inherits from Rectangle.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square: inheirts from Rectangle
+    """
+
+    def __init__(self, size):
+        """Method: construct rectangle
+
+        Args:
+        size(int): size of rectangle
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Method: returns a string with the area
+        """
+        return super().area()
+
+    def __str__(self):
+        """Method: returns a printable string
+        """
+        return ("[Square] {}/{}".format(self.__size, self.__size))
