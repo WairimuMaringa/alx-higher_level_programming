f9f14afbe699c6255865fa99338ccecc67b96f6a
WairimuMaringa <makena96wairimu@gmail.com>
2022-09-26 07:19:02 -0700

    task six

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""An emoty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """Class: BaseGeometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
