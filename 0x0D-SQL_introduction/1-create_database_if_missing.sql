4b41338bbe60f4fc07527bf1ebbc9067cc560922
WairimuMaringa <makena96wairimu@gmail.com>
2022-11-01 07:16:33 -0700

    task zero to four

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Script that creates the database hbtn_0c_0 in a MySQL server
+-- If the database exists, the script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Script that deletes the database hbtn_0c_0 in a MySQL server
+-- If database exists, script should not fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
