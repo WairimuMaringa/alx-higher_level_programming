0af8370b4f4f1f2b0f3181557191e397e0ed6740
WairimuMaringa <makena96wairimu@gmail.com>
2022-11-02 08:52:49 -0700

    task ten

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- import the database dump from hbtn_0d_tvshows to your MySQL server
 -- write a script displaying all shows in that database that have at least one genre linked
 -- each record should display tv_shows.title tv_show_genres.genre.id and sorted in ascending order
-SELECT tv_shows.title, tv_show_genres.genre.id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
