e07b9953a8c60a3e1a7dc4c58cab0f90e95df5ec
WairimuMaringa <makena96wairimu@gmail.com>
2022-11-02 08:54:27 -0700

    task ten again

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,5 @@
 -- each record should display tv_shows.title tv_show_genres.genre.id and sorted in ascending order
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
