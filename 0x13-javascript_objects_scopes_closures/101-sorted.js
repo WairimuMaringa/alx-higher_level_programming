4bed9986cf6c3307ead3aa37a8c26036ddb9ae9a
WairimuMaringa <makena96wairimu@gmail.com>
2022-11-29 06:19:37 -0800

    advanced two

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const newDict = {};
+
+Object.keys(dict).map(function (key) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+  return (true);
+});
+
+console.log(newDict);
