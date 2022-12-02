554be43a69bbe1c4b35cd2d3287688edc17aae17
WairimuMaringa <makena96wairimu@gmail.com>
2022-12-02 05:09:55 -0800

    task six

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Class:
+    a) State.
+"""
+from sqlchemy import Column, Integer, String, MetaData
+from sqlachemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """ Class:
+    contains the class definition of state and base.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
