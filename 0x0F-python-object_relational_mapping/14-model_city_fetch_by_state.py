1f5b8fd77c1f40445c27ad431dd53fc9f17d7d15
WairimuMaringa <makena96wairimu@gmail.com>
2022-12-02 07:23:17 -0800

    fourteen

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+List all state objects from a database.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def mysqlconnect(username, password, database, port=3306):
+    """Function: connect ot MySQL
+
+    Args:
+    username(str): username to connect to MySQL
+    password(str): password to connect to MySQL
+    database(str): databse to connect to MySQL
+    port(int): port to connect to MySQL
+
+    Return:
+    connect to MySQL
+    """
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
+                username,
+                password,
+                port,
+                database),
+            pool_pre_ping=True)
+    bd_session = sessionmaker(bind=engine)
+    return bd_session()
+
+
+if __name__ == '__main__':
+    bd_session = mysqlconnect(sys.argv[1], sys.argv[2], sys.argv[3])
+
+    items = bd_session.query(
+            State,
+            City
+            ).filter(State.id == City.stae_id).all()
+
+    for state, city in items:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
