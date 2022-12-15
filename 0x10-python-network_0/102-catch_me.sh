1ab6f0948c267bdb69ed75d91e5188ce694f8250
WairimuMaringa <makena96wairimu@gmail.com>
2022-12-15 06:29:57 -0800

    once again

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request and give a response
-curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "You got me!" -L
+curl -sX PUT 0.0.0.0:5000/catch_me -d -H "You got me!" -L
