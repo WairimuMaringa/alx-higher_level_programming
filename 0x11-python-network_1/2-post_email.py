f05ab15557f470ef9d894efcd70b0af47f642808
WairimuMaringa <makena96wairimu@gmail.com>
2022-12-16 06:51:39 -0800

    task two

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that takes url in an email, sends a post request
+and displays body of the response
+"""
+from urllib import request, parse
+import sys
+
+
+if __name__ == "__main__":
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
