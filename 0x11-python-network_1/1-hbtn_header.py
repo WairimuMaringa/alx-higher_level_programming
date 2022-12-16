6b7936c8b6ebe942398ecc9647656c9afe1ad8a7
WairimuMaringa <makena96wairimu@gmail.com>
2022-12-16 06:43:40 -0800

    one

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,7 @@ import urllib.request
 import sys
 
 
-if __name__ "__main__":
+if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         html = response.info()
         print(html.get('X-Request-Id'))
